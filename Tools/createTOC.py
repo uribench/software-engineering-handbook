2918d7a6caa206f91dfaed174b3bd63ad1bd320e
uribench <uribench@gmail.com>
2018-05-22 12:43:00 +0000

    Expose command line interface

--- Tools/createTOC.py
+++ Tools/createTOC.py
@@ -1,38 +1,53 @@
 # createTOC.py
 # MIT license; Copyright (C) 2018 Uri Benchetrit
-#
-# Traverse a directory tree and generate an ordered linked validated TOC
 
+"""Traverse a directory tree and generate an ordered linked validated TOC.
+
+Usage:
+  createTOC.py [options] [TOC]
+
+Arguments:
+  TOC  destination TOC filename
+
+Options:
+  -h --help             show this help message and exit
+  -v --verbose          print warning messages
+  --dirs-root=PATH      directories tree root relative to program location [default: ../Training/] 
+  --toc-path=PATH       TOC file path relative to program location [default: ../]
+  --links-root=PATH     TOC file links root relative to TOC file location [default: ./Training/]
+  --include=PATTERNS    include files or directories which match these comma
+                        separated patterns [default: .md]
+  --exclude=PATTERNS    exclude files or directories which match these comma
+                        separated patterns [default: .git]
+  -d --depth=LEVEL      max depth of the generated TOC tree [default: 8]
+  --start-index=INDEX   start index of the TOC items [default: 1]
+  --no-item-prefix      do not include item prefix for the TOC items
+  --no-index            do not include index numbers for the TOC items
+  --no-item-link        do not include links for the TOC items
+  --header              include HTML header for the TOC file
+"""
+
+from docopt import docopt
 import os
 import sys
 from urllib.request import pathname2url
 
-dirsTreeRoot = '../Training/'   # relative to 'Tools/' directory
-tocPath = '../'                 # relative to 'Tools/' directory
-tocLinksRoot = './Training/'    # relative to 'tocFile' location
 tocFile = 'TOC.md'
 tocHeader = '<style>ul { list-style-type: none; }</style>\n\n'  # kill bullets of unordered list
 tocTitle = '## Table of Contents\n\n'
 markdownUL = '-'
 dirOrderFile = '.dirorder'
-whileList = ['.md']
+whiteList = ['.md']
 blackList = [dirOrderFile, tocFile, sys.argv[0]]
-tocMaxLevel = 8
-initialIndex = 1
-includeIndex = True
-includeItemPrefix = True
-includeItemLink = True
-displayWarnings = True
-includeTocHeader = False
 
 def createTOC(path, tocFileObject, level, index):
     """"""
-    if level >= tocMaxLevel:
+    if level >= maxDepth:
         return
 
     dirOrderFileList = dirOrderFileToList(path)
     fileList = os.listdir(path)
-    fileList = filterFiles(path, fileList, whileList)
+    fileList = filterFiles(path, fileList)
     if not validateIntegrity(path, dirOrderFileList, fileList):
         sys.exit()
 
@@ -47,14 +62,14 @@ def createTOC(path, tocFileObject, level, index):
             createTOC(filePath, tocFileObject, level + 1, index + [1])
         index[level] += 1
 
-def filterFiles(path, fileList, whileList):
+def filterFiles(path, fileList):
     """"""
     ignoreList = []
     for fileName in fileList:
         filePath = os.path.join(path, fileName)
         if not os.path.isdir(filePath):
             extension = os.path.splitext(fileName)[1]
-            if extension not in whileList:
+            if extension not in whiteList:
                 ignoreList.append(fileName)
 
     return list(set(fileList) - set(ignoreList + blackList))
@@ -66,7 +81,7 @@ def dirOrderFileToList(path):
         if not os.path.exists(dirOrderFullFileName):
             # allow missing dirOrderFile for an empty directory 
             # note: be aware that git is a content tracker and empty directories are not content
-            if displayWarnings and not os.listdir():
+            if verbose and not os.listdir():
                 print('Warning: Expected <{}> file does not exit in <{}>'.format(dirOrderFile, path))
             return []
 
@@ -119,6 +134,31 @@ def formatTOC(path, fileName, level, index):
 
     return '{}{}{}{}\n'.format(indent, itemPrefix, indexString, tocItem)
 
+def processArgs():
+    global verbose, maxDepth, startIndex, includeItemPrefix, includeIndex
+    global includeItemLink, includeTocHeader, whiteList, blackList
+    global dirsTreeRoot, tocPath, tocLinksRoot, tocFile
+
+    args = docopt(__doc__)
+
+    print(args)
+
+    verbose = args['--verbose']
+    maxDepth = int(args['--depth'])
+    startIndex = int(args['--start-index'])
+    includeItemPrefix = not args['--no-item-prefix']
+    includeIndex = not args['--no-index']
+    includeItemLink = not args['--no-item-link']
+    includeTocHeader = args['--header']
+    whiteList = list(set(whiteList + args['--include'].split(',')))
+    blackList = list(set(blackList + args['--exclude'].split(',')))
+    dirsTreeRoot = args['--dirs-root']
+    tocPath = args['--toc-path']
+    tocLinksRoot = args['--links-root']
+
+    if args['TOC'] is not None:
+        tocFile = args['TOC']
+
 def main():
     tocFullFileName = os.path.join(tocPath, tocFile)
     if os.path.exists(tocFullFileName):
@@ -132,10 +172,11 @@ def main():
             fo.write(tocTitle)
             root = dirsTreeRoot
             level = 0
-            index = [initialIndex]
+            index = [startIndex]
             createTOC(root, fo, level, index)
     except IOError as e:
         print('Error: Operation failed: {}'.format(e.strerror))
 
 if __name__ == '__main__':
+    processArgs()
     main()
