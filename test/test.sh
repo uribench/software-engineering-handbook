a10f4c3f731e95e0125061210b4ff2b9a469161f
uribench <uribench@gmail.com>
2019-05-22 12:30:16 +0000

    Set execution permission

--- test/test.sh
+++ test/test.sh
@@ -2,6 +2,7 @@
 
 if [[ ! -f censor.sh ]]; then
   wget "https://raw.githubusercontent.com/uribench/censor/master/censor.sh"
+  chmod +x censor.sh
 fi
 
 ./censor.sh blacklist.txt.gpg ../Guides/ ../Topics/ ../config/
